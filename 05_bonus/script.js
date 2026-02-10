02891307fd147f4840ded079cff23081c567c29c
Francesca Golzio <francescagolzio@gmail.com>
2026-02-10 20:46:49 +0100

    complete 05 bonus

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -5,13 +5,44 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
+const userName = 'Mario';
 
+const dateNow = new Date;
+const hourNow = dateNow.getHours();
+//console.log(hourNow);
+
+let greetings;
 
 // Dichiara la funzione qui.
 
+/* accetta...       una stringa 👇 contenente un nome */
+function greetingsByDayMoment(nome) {
+
+  /* controlla l'ora e restituisce un saluto */
+    /* "buongiorno" se è mattina (fino alle 13) */
+    if (hourNow < 13) {
+        /* un saluto seguito dal 👇 nome fornito */
+      greetings = `buongiorno ${nome}`;
+      return greetings;      
+    }
+    
+    /* "buon pomeriggio" se è pomeriggio (fino alle 17) */
+    if (13 > hourNow || hourNow < 17) {
+      greetings = `buon pomeriggio ${nome}`;      
+      return greetings;      
+    } 
+    
+    /* "buonasera" se è sera (oltre le 17) */
+    if (hourNow > 17) {
+      greetings = `buonasera ${nome}`;      
+      return greetings;
+    }
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
+greetingsByDayMoment(userName)
+console.log(greetingsByDayMoment(userName));
 
 
 
