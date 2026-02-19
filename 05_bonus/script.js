5f5f4d07f71ba1df506ccea8556687cc9da67e2f
Francesca Golzio <francescagolzio@gmail.com>
2026-02-19 18:47:18 +0100

    fix 05 bonus
    - move const declaration inside the function;
    - fix afternoon condition

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -7,33 +7,33 @@ e buonasera se è sera (oltre le 17)
 
 const userName = 'Mario';
 
-const dateNow = new Date;
-const hourNow = dateNow.getHours();
-//console.log(hourNow);
-
 let greetings;
 
 // Dichiara la funzione qui.
 
 /* accetta...       una stringa 👇 contenente un nome */
 function greetingsByDayMoment(nome) {
+  
+  const dateNow = new Date;
+  const hourNow = dateNow.getHours();
+  //console.log(hourNow);
 
   /* controlla l'ora e restituisce un saluto */
     /* "buongiorno" se è mattina (fino alle 13) */
-    if (hourNow < 13) {
+    if (hourNow <= 13) {
         /* un saluto seguito dal 👇 nome fornito */
       greetings = `buongiorno ${nome}`;
       return greetings;      
     }
     
     /* "buon pomeriggio" se è pomeriggio (fino alle 17) */
-    if (13 > hourNow || hourNow < 17) {
+    if (hourNow < 17) { 
       greetings = `buon pomeriggio ${nome}`;      
       return greetings;      
     } 
     
     /* "buonasera" se è sera (oltre le 17) */
-    if (hourNow > 17) {
+    if (hourNow >= 17) {
       greetings = `buonasera ${nome}`;      
       return greetings;
     }
